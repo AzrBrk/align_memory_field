c6a0b5603b23b037bb35e161b4ae41616f309015
AzrBrk <pbrink51@gmail.com>
2024-02-08 05:26:31 +0800

    Update align_memory_field.hpp

--- align_memory_field.hpp
+++ align_memory_field.hpp
@@ -62,6 +62,7 @@ public:
     align_memory_field(align_memory_field&& another)
     {
         base_ptr = another.base_ptr;
+        another.base_ptr = nullptr;
     }
 
     template<typename structT> structT* cast() const
