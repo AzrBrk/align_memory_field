ec67866984060a60cbfc41df9e2106c9f12a1d2b
AzrBrk <pbrink51@gmail.com>
2024-02-10 07:14:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,48 +3,69 @@
 #include<numeric>
 #include<algorithm>
 #include<iostream>
+#include<string>
 #include"align_memory_field.hpp"
 #include<benchmark/benchmark.h>
 
 
 
-
-static void foo_tuple(benchmark::State& state)
-{
-    std::tuple<int, double, char> tp{};
-    for (auto _ : state) {
-        std::get<0>(tp) = 10;
-        std::get<1>(tp) = 2.33;
-        std::get<2>(tp) = 'l';
-        std::get<0>(tp); std::get<1>(tp); std::get<2>(tp);
-    }
-
-}
-
-void foo_align_offset_f(benchmark::State& state)
+void foo_align_offset_f4(benchmark::State& state)
 {
 
-    align_memory_field<8, int, double, char> m_field{};
+    align_memory_field<4, int, std::string, int> m_field{};
+    m_field.initialize<1>("");
     for (auto _ : state) {
         m_field.template write<0>(10);
-        m_field.template write<1>(2.33);
-        m_field.template write<2>('l');
+        m_field.template write<1>("very long hello worldhello worldhello world");
+        m_field.template write<2>(99);
         m_field.read<0>(); m_field.read<1>(); m_field.read<2>();
     }
 
 
 }
-BENCHMARK(foo_align_offset_f);
+void foo_align_offset_f8(benchmark::State& state)
+{
+
+    align_memory_field<8, int, std::string, int> m_field{};
+    m_field.initialize<1>("");
+    for (auto _ : state) {
+        m_field.template write<0>(10);
+        m_field.template write<1>("very long hello worldhello worldhello world");
+        m_field.template write<2>(99);
+        m_field.read<0>(); m_field.read<1>(); m_field.read<2>();
+    }
+
+
+}
+static void foo_tuple(benchmark::State& state)
+{
+    std::tuple<int, std::string, int> tp{};
+    for (auto _ : state) {
+        std::get<0>(tp) = 10;
+        std::get<1>(tp) = "very long hello worldhello worldhello world";
+        std::get<2>(tp) = 99;
+        std::get<0>(tp); std::get<1>(tp); std::get<2>(tp);
+    }
+
+}
+BENCHMARK(foo_align_offset_f4);
+BENCHMARK(foo_align_offset_f8);
 BENCHMARK(foo_tuple);
-BENCHMARK(foo_align_offset_f);
+
+BENCHMARK(foo_align_offset_f4);
+BENCHMARK(foo_align_offset_f8);
 BENCHMARK(foo_tuple);
-BENCHMARK(foo_align_offset_f);
+
+BENCHMARK(foo_align_offset_f4);
+BENCHMARK(foo_align_offset_f8);
 BENCHMARK(foo_tuple);
-BENCHMARK(foo_align_offset_f);
+
+BENCHMARK(foo_align_offset_f4);
+BENCHMARK(foo_align_offset_f8);
 BENCHMARK(foo_tuple);
-BENCHMARK(foo_align_offset_f);
-BENCHMARK(foo_tuple);
-BENCHMARK(foo_align_offset_f);
+
+BENCHMARK(foo_align_offset_f4);
+BENCHMARK(foo_align_offset_f8);
 BENCHMARK(foo_tuple);
 
 BENCHMARK_MAIN();
