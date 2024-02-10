25620292eeaaf04964bbb44fab945e90d8e94bd2
AzrBrk <pbrink51@gmail.com>
2024-02-11 01:52:44 +0800

    align_offset_ptr增加数组的支持
    对align_offset_ptr增加对数组的计算，但是尚未应用到align memory field

--- offset_pointer.hpp
+++ offset_pointer.hpp
@@ -5,13 +5,24 @@ namespace offset_pointer {
     {
         return *(reinterpret_cast<T*>(reinterpret_cast<unsigned char*>(base_ptr) + offset));
     }
-    //编译时计算
-    //offset_ptr模拟成员在结构体中的分布， advance：跨越一个指定的size， seek:寻找下一个成员的首地址
+    //to fix the std::conditional_t with SFINAE
+    template<bool con, class T> struct invoke_type_if_impl
+    {
+        using type = T;
+    };
+
+    template<class T> struct invoke_type_if_impl<true, T>
+    {
+        using type = typename T::type;
+    };
+
+    template<bool con, class T> using invoke_type_if = typename invoke_type_if_impl<con, T>::type;
+
+    //stimulate members layout in a structure
     template<std::size_t pack_size, std::size_t layer_offset, std::size_t offset> struct align_offset_ptr
     {
-        static_assert(pack_size <= 8, "An alignment size exceeds 8 is not tested!");
         static constexpr std::size_t layer = layer_offset;//alignment layer as base
-        static constexpr std::size_t value = offset;//当前层偏移指针
+        static constexpr std::size_t value = offset;//offset from the current layer
         template<std::size_t lay> struct advance_layer
         {
             using type = align_offset_ptr<pack_size, layer + lay, 0>;
@@ -41,112 +52,104 @@ namespace offset_pointer {
             >;
         };
         template<std::size_t advance_offset> using advance = typename advance_impl<advance_offset>::type;
-        template<class structT, class T>
-        static T& get(structT* base_ptr)
-        {
-            using seek_ptr = seek<sizeof(T)>;
-            return read_from_align_offset<structT, T>(base_ptr, seek_ptr::layer * pack_size + seek_ptr::value);
-        }
-    };
-    //递归迭代，实现类似std::get的方法
-    template<std::size_t N, class offset_pointer, class TL> struct offset_iter {};
-    template<std::size_t N, class offset_pointer, template<class ...> class TL, class first, class ...rest>
-    struct offset_iter<N, offset_pointer, TL<first, rest...>>
-    {
-        using recurse_type = offset_iter<N - 1,
-            typename offset_pointer
-            ::template seek<sizeof(first)>
-            ::template advance<sizeof(first)>, TL<rest...>>;
-        using iter_type = typename recurse_type::iter_type;
-        using type = typename recurse_type::type;
-
-    };
-    template<class offset_pointer, template<class...> class TL, class first, class ...rest>
-    struct offset_iter<0, offset_pointer, TL<first, rest...>>
-    {
-        using type = first;
-        using iter_type = offset_pointer;
-    };
-    //封装成函数
-    template<std::size_t I, class CTL, class T> auto& get_member(T* x)
-    {
-        using types_iter = offset_iter<I, align_offset_ptr<alignof(T), 0, 0>, CTL>;
-
-        return types_iter::iter_type::template get<T, typename types_iter::type>(x);
-    }
-    template<std::size_t I, class CTL, class T> void* get_pointer(T* x)
-    {
-        using types_iter = offset_iter<I, align_offset_ptr<alignof(T), 0, 0>, CTL>;
-        using explore_offset = typename types_iter::iter_type::template seek<sizeof(typename types_iter::type)>;
-
-        return reinterpret_cast<void*>(
-            reinterpret_cast<unsigned char*>(x) + explore_offset::layer * alignof(T) + explore_offset::value
-            );
-    }
-
-    //运行时计算
-    template<typename structT, typename T>
-    std::size_t align_offset_seek(std::size_t& layer, std::size_t& offset)
-    {
-        constexpr std::size_t pack_size = alignof(structT);
-        constexpr std::size_t offset_size = sizeof(T);
-        if (offset % offset_size) {
-            offset += 1;
-            if (offset + offset_size > pack_size) {
-                layer += 1;
-                offset = 0;
-            }
-            return align_offset_seek<structT, T>(layer, offset);
-        }
-
-        return pack_size * layer + offset;
-    }
-
-    template<typename structT, typename T>
-    void align_offset_advance(std::size_t& layer, std::size_t& offset)
-    {
-        constexpr std::size_t pack_size = alignof(structT);
-        constexpr std::size_t advance_size = sizeof(T);
-        if (offset + advance_size < pack_size) {
-            offset += advance_size;
-        }
-        else {
-            layer += advance_size / pack_size;
-            layer += advance_size % pack_size ? 1 : 0;
-            offset = 0;
-        }
-    }
-
-
-    template<class F, class TT> struct init_and_call
-    {
-        F f;
-        TT* x;
-        inline static std::size_t value = 0;
-        inline static std::size_t layer = 0;
-
-        template<class T>
-        operator T() {
-            const std::size_t off = align_offset_seek<TT, T>(layer, value);
-            f(read_from_align_offset<TT, T>(x, off));
-            align_offset_advance<TT, T>(layer, value);
-            return T{};
-        };
+       
+        static constexpr std::size_t offset_v = layer * pack_size + value;
     };
 
-    template<class F, class T, class first, class ...rest> constexpr void for_each_member_impl(F f, T* x)
+ 
+    //align offset iterator impl
+    template<class align_offset_pointer, class T>
+    struct align_offset_advance_basic
     {
-        if constexpr (!requires{ T{ first{}, rest{}... }; }) {
-            T{ rest{f, x}... };
-        }
-        else {
-            for_each_member_impl<F, T, init_and_call<F, T>, first, rest...>(f, x);
-        }
-    }
+        using type = typename align_offset_pointer
+            ::template seek<sizeof(T)>
+            ::template advance<sizeof(T)>;
+    };
 
-    template<class F, class T> constexpr void for_each_member(F f, T& x)
+    template<class align_offset_pointer, class T>
+    using align_offset_advance_basic_t =
+        typename align_offset_advance_basic<align_offset_pointer, T>::type;
+
+    //array in structure is considered as N members
+    template<class align_offset_pointer, class array_t> struct align_offset_advance_array_impl {};
+    template<class align_offset_pointer, class basic_type, std::size_t N>
+    struct align_offset_advance_array_impl<align_offset_pointer, basic_type[N]>
     {
-        for_each_member_impl<F, T, init_and_call<F, T>>(f, &x);
-    }
+        using type = typename align_offset_advance_array_impl<
+            align_offset_advance_basic_t<align_offset_pointer, basic_type>,
+            basic_type[N - 1]>::type;
+    };
+    template<class align_offset_pointer, class basic_type>
+    struct align_offset_advance_array_impl<align_offset_pointer, basic_type[1]>
+    {
+        using type = align_offset_advance_basic_t<align_offset_pointer, basic_type>;
+    };
 
+    template<class align_offset_pointer, class array_t>
+    using align_offset_advance_array_t = typename align_offset_advance_array_impl<
+        align_offset_pointer, array_t
+    >::type;
+
+    template<class align_offset_pointer, class T> 
+    using align_offset_advance_t = std::conditional_t<
+        std::is_array_v<T>,
+        invoke_type_if<std::is_array_v<T>, align_offset_advance_array_impl<align_offset_pointer, T>>,
+        invoke_type_if<!std::is_array_v<T>, align_offset_advance_basic<align_offset_pointer, T>>
+    >;
+
+    
+    template<class align_offset_pointer, class T> struct align_offset_seek_impl
+    {
+        using type = std::conditional_t<std::is_array_v<T>,
+            typename align_offset_pointer::template seek<sizeof(std::remove_extent_t<T>)>,
+            typename align_offset_pointer::template seek<sizeof(T)>
+        >;
+    };
+
+    template<class align_offset_pointer, class T>
+    using align_offset_seek_t = typename align_offset_seek_impl<align_offset_pointer, T>::type;
+
+    template<class align_offset_pointer, class offset_map, std::size_t N>
+    struct align_offset_iterator
+    {
+        using type = align_offset_pointer;
+    };
+
+    template<class align_offset_pointer, std::size_t N,
+        template<class ...> class offset_map, class first, class ...rest>
+    struct align_offset_iterator<align_offset_pointer, offset_map<first, rest...>, N>
+    {
+        using recursive_type = align_offset_iterator<
+            align_offset_advance_t<align_offset_pointer, first>,
+            offset_map<rest...>, N - 1>;
+        using type = typename recursive_type::type;
+        using get = typename recursive_type::get;
+        using value_type = typename recursive_type::value_type;
+    };
+    template<class align_offset_pointer, template<class...> class offset_map, class first, class ...rest>
+    struct align_offset_iterator<align_offset_pointer, offset_map<first, rest...>, 0>
+    {
+        //kept pointer
+        using type = align_offset_pointer;
+        //common iterface
+        using get = align_offset_seek_t<type, first>;
+        //common interface for tyinfo
+        using value_type = first;
+    };
+
+    template<class ...> struct offset_pattern {};
+    template<std::size_t pack_size, class ...typs> 
+    struct offset_vector
+    {
+        template<std::size_t I>
+        using iterator = align_offset_iterator<
+            align_offset_ptr<pack_size, 0, 0>,
+            offset_pattern<typs...>, I>;
+
+        template<std::size_t I> 
+        static constexpr std::size_t offset_v = iterator<I>::get::offset_v;
+
+        template<std::size_t I> 
+        using value_type = typename iterator<I>::value_type;
+    };
 }
