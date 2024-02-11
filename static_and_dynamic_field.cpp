12301c94970e71d17312a6fde3bfb643dec99995
AzrBrk <pbrink51@gmail.com>
2024-02-11 22:51:24 +0800

    Add files via upload

---
+++ static_and_dynamic_field.cpp
@@ -0,0 +1,42 @@
+#include"align_memory_field.hpp"
+#include<iostream>
+
+auto do_nothing = [](unsigned char* p) {};
+
+struct X {
+    int a;
+    char str[20]{ "hello world" };
+    int arr[10]{};
+    double c;
+};
+
+template<class T, std::size_t N> constexpr void print_array(T (&arr) [N])
+{
+    for (const auto& i: arr) {
+        std::cout << i;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    X x{};
+    align_memory_field<8, 
+        decltype(do_nothing), 
+        int, char[20], int[10], double
+    > static_field{ &x };
+
+    auto& ch_arr_ref = static_field.read<1>();
+    auto& int_arr_ref = static_field.read<2>();
+
+    for (auto i = 0; i < 10; ++i) {
+        int_arr_ref[i] = i;
+    }
+
+    align_memory_field<8, int, char[20], int[10], double> dynamic_field{};
+
+    copy_struct<X>(dynamic_field, std::move(*static_field.cast<X>()));
+
+    [&] <std::size_t ...I>(std::index_sequence<I...>){
+        (print_array(dynamic_field.template read<I>()), ...);
+    }(std::index_sequence<1,2>{});
+}
