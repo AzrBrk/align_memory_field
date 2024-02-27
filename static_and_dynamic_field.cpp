a11907e3fdee076a1274785dd6d13eab312310d2
AzrBrk <pbrink51@gmail.com>
2024-02-28 04:19:46 +0800

    Update static_and_dynamic_field.cpp

--- static_and_dynamic_field.cpp
+++ static_and_dynamic_field.cpp
@@ -21,12 +21,12 @@ template<class T, std::size_t N> constexpr void print_array(T (&arr) [N])
 int main() {
     X x{};
     align_memory_field<8, 
-        decltype(do_nothing), 
+        decltype(do_nothing), //do nothing because it is not a allocated memory blocked in heap
         int, char[20], int[10], double
     > static_field{ &x };
 
-    auto& ch_arr_ref = static_field.read<1>();
-    auto& int_arr_ref = static_field.read<2>();
+    auto& ch_arr_ref = static_field.read<1>();//get ref of X::str
+    auto& int_arr_ref = static_field.read<2>();//get ref of X::arr
 
     for (auto i = 0; i < 10; ++i) {
         int_arr_ref[i] = i;
